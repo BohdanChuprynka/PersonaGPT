6487e40c4e1a5d49e24d3118b56272744e83b391
Bohdan Chuprynka <ChuprinkaBogdan223@gmail.com>
2024-08-16 23:12:22 -0400

    Summarized Files

---
+++ DataCollector.py
@@ -0,0 +1,32 @@
+def optimize_messages(messages):
+      """
+      Function which uses a set of tuning algorithms to meet the criteria of optimized data for future models.
+      """
+
+      # TODO: Include only messages in ukrainian language
+
+
+      # TODO: Put todos below in order of priority 
+      # For each of the points below, if true: add one, if false: minus one
+      # TODO: Add detection system for context and response:
+
+      # TODO: If the message contains question mark in the end of the message, it is a context
+      # TODO: The first message of the new day is probably a context.
+      # TODO: If there are few messages in a row from user, concatenate them into one message.
+      # TODO: If there is a significant time gap (e.g., several hours) between messages, the first message after the gap might be a context.
+      # TODO: Look for specific keywords or phrases that typically indicate a context (e.g., "What do you think about...", "Can you explain...", "Why is...").
+      # TODO: If a message is a direct reply to a previous context message, it is likely a response.
+      # TODO: Short messages that directly follow a context are likely responses.
+      # TODO: If the same user repeatedly sends messages ending with question marks or messages at the start of the day, those are likely contexts.
+       
+
+import parsers.telegram_parse as telegram_parse
+import asyncio
+async def main(): 
+      print("Started")
+      await telegram_parse.main()
+
+if __name__ == "__main__": 
+       loop = asyncio.get_event_loop()
+       loop.run_until_complete(main())
+      
---
+++ parsers/telegram_parse.py
@@ -0,0 +1,154 @@
+# Python file specifically for DataCollector.py. '
+from telethon import TelegramClient
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.types import User, PeerUser
+from telethon.errors import FloodWaitError
+import asyncio
+import time 
+import openai
+from dotenv import load_dotenv
+import json
+import os
+import sys
+
+dotenv_path = ".env"
+load_dotenv(dotenv_path=dotenv_path)
+
+api_id = os.getenv('TELEGRAM_API_ID')
+api_hash = os.getenv('TELEGRAM_HASH_ID')
+phone_number = os.getenv('PHONE_NUMBER')
+my_telegram_id = os.getenv('my_telegram_id')
+session_name = "telegram_parser"
+client = TelegramClient(session_name, api_id, api_hash)
+
+async def extract_message_info(messages):
+      extracted_dialog = []
+      last_message=None
+
+      for message in messages:
+            try: 
+                  text = message.message.strip() if message.message else ""
+                  sender = message.from_id if message.from_id else (await client.get_entity(message.peer_id)).id
+                  sender = sender.user_id if isinstance(sender, PeerUser) else sender # Deletes PeerUser classes and keeps only int id
+
+                  #sent_by_me = my_telegram_id == sender   
+                  date = message.date 
+            except FloodWaitError as e:
+                  print(f"FloodWaitError: sleeping for {e.seconds} seconds.")
+                  await asyncio.sleep(e.seconds)
+                  continue 
+                  
+            if text:
+                  if last_message and sender == last_message[1]:
+                        last_message[0] = " ".join([last_message[0], text])
+                  else:
+                        if last_message:
+                              extracted_dialog.append(last_message)
+                        last_message = [text, sender, date]
+
+      if last_message:
+            extracted_dialog.append(last_message)
+      
+      return extracted_dialog
+
+
+
+import pandas as pd
+
+async def parse_data(threshold: int =50, 
+                     message_limit=None,
+                      dialogs_limit: int = 100,
+                      verbose=1,
+                      checkpoints: bool = True):
+    """
+    Parses all the messages in the profile.
+    
+    Args:
+        threshold: int
+            The minimum amount of messages in a dialog to be processed.
+        message_limit: int
+            The maximum amount of messages to be processed in a dialog.
+        dialogs_limit: int
+            The maximum amount of dialogs to be processed.
+        verbose: int
+            The amount of output to be printed.
+        top_chats_first: bool
+            Whether to process chats with most messages first.
+
+    Returns:
+        pd.DataFrame
+            The parsed data.
+    """
+    async with client:
+        # TODO: Implement checkpoint system
+            # Don't forget about assigning filtered_dialogs
+            # You can implement in this structure: filtered_dialogs, itered_dialogs.
+
+
+        dialogs = await client.get_dialogs()
+        dialogs = [dialog for dialog in dialogs if isinstance(dialog.entity, User)]
+        dialogs = [dialog for dialog in dialogs if not dialog.entity.bot]
+        dialogs = dialogs[:dialogs_limit]
+        filtered_dialogs = pd.DataFrame(columns=["Message", "Sender","Date"])
+
+        if verbose: 
+            total = 0
+            print(f"Total dialogs: {len(dialogs)}")
+
+        # Check for checkpoint
+        if checkpoints:
+            if os.path.exists("checkpoint.pkt"):
+                checkpoint = pd.read_pickle("checkpoint.pkt")
+                filtered_dialogs = checkpoint["data"]
+                total = checkpoint["last_iter"]
+                dialogs = dialogs[total-1:]
+                print(f"Resuming from checkpoint. Dialogs left: {len(dialogs)}.")
+
+        # Main loop of the function
+        for dialog in dialogs[:dialogs_limit]:
+            start_time = time.time() if verbose else None
+            messages_info = []
+            async for message in client.iter_messages(dialog.entity, limit=message_limit, wait_time=10):
+                messages_info.append(message)
+
+            total_messages = len(messages_info)
+            if total_messages > threshold:
+                extracted_dialog = await extract_message_info(messages_info)
+                filtered_dialogs = pd.concat([filtered_dialogs, pd.DataFrame(extracted_dialog, columns=["Message", "Sender", "Date"])])
+                if verbose: 
+                    total += 1
+                    run_time = time.time() - start_time
+                    print(f"Dialogs processed: {total}, left: {len(dialogs) - total}. Run time: {run_time:.2f} seconds") 
+            if checkpoints:
+                checkpoint = {"data": filtered_dialogs,
+                               "last_iter": total}
+                pd.to_pickle(checkpoint, "checkpoint.pkt")
+        if os.path.exists("checkpoint.pkt"):
+            os.remove("checkpoint.pkt")
+        
+        return filtered_dialogs
+    
+async def main():
+    if os.path.exists(f"parsers\{session_name}.session-journal"):
+        print(f"Session {session_name} exists. Please delete it and restart the script. Or change the session name in the script.")
+        sys.exit()
+    else:
+        await client.start(phone_number)
+        print(f"Connecting with {client.session}")
+        data = await parse_data(message_limit=100, dialogs_limit=10, verbose=1, checkpoints=True)
+        data = pd.DataFrame(data, columns=["Message", "Sender", "Date"])
+        data["Sent_by_me"] = int(my_telegram_id) == data["Sender"]
+        return data
+        client.disconnect()
+
+        if os.path.exists("parsers/full_telegram_data.csv"):
+            print("File with the same name already exists. Do you want to overwrite it? (y/n)")
+            if input() == "y":
+                  data.to_csv(r'full_telegram_data.csv', index=False)
+            else:
+                  print("File not overwritten.")
+                  sys.close()
+        print("DONE")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
