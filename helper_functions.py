f41f4f434b595ea5659181c205e40c7bc83cad63
Bohdan Chuprynka <ChuprinkaBogdan223@gmail.com>
2025-01-03 12:34:49 -0500

    Now script automatically recognizes paths. SO GOOD!

--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def find_repository_folder(start_path: str = None) -> str:
     """
@@ -27,4 +28,49 @@ def find_repository_folder(start_path: str = None) -> str:
         # Move up one level
         current_path = parent_path
 
+def find_files(base_dir, pattern: str = None) -> list:
+    """
+    Automatically locates specific files in base_dir based on pattern.
+
+    Parameters:
+    - base_dir (str): The starting directory for the search.
+    - patterns (str): Name of the file
+
+    Returns:
+    - list: A list of file paths that match the specified pattern.
+    """
+    if pattern is None:
+        pattern = r'.*'  # Default: Match all files
+
+    found_files = []
+    regex = re.compile(pattern)
+    for root, _, files in os.walk(base_dir):
+        for file in files:
+            if regex.match(file):
+                found_files.append(str(os.path.join(root, file)))
+
+    return found_files
+    
+def find_dirs(base_dir, pattern: str = None) -> list: 
+    """
+    Automatically locates specific directory in base_dir based on pattern.
+
+    Parameters:
+    - base_dir (str): The starting directory for the search.
+    - patterns (str): Name of the directory
+
+    Returns:
+    - list: A list of directory paths that match the specified pattern. 
+    """
+    if pattern is None:
+        pattern = r'.*'  # Default: Match all directories
+
+    found_dirs = []
+    regex = re.compile(pattern)
+    for root, dirs, _ in os.walk(base_dir):
+        for dir_name in dirs:
+            if regex.match(dir_name):
+                found_dirs.append(str(os.path.join(root, dir_name)))
+
+    return found_dirs
 
